e2f0955516760cea86fc3eb18051f73f418987f0
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-11-05 14:12:44 -0800

    Cars model done, now on to CRUD

---
+++ cars/carsModel.js
@@ -0,0 +1,21 @@
+const db = require("../data/db");
+module.exports = {
+  get,
+  getById,
+  insert
+};
+function get() {
+  return db("cars");
+}
+function getById(id) {
+  return db("cars")
+    .where({ id })
+    .first();
+}
+function insert(car) {
+  return db("cars")
+    .insert(car)
+    .then(([id]) => {
+      return getById(id);
+    });
+}
